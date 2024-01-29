a3b2b5f3ece917ae36b80fc828693af66e27c1c1
TranDucAn <ducantran11@gmail.com>
2024-01-29 12:06:34 +0700

    admin - display and delete

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -23,4 +23,23 @@ adminRouter.post("/admin/add-product", admin, async (req, res,) => {
     }
 
 });
+//get product
+adminRouter.get('/admin/get-products', async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.json(products);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+})
+//delete product
+adminRouter.post("/admin/delete-product", admin, async (req, res) => {
+    try {
+        const { id } = req.body;
+        let product = await Product.findByIdAndDelete(id);
+        res.json(product);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
 module.exports = adminRouter;
